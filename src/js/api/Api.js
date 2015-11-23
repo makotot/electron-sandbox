23342ce172c6694cff5d6c157fab68ba65ef75cc
makotot <makoto.ttn@gmail.com>
2015-11-23 23:42:02 +0900

    * param

--- src/js/api/Api.js
+++ src/js/api/Api.js
@@ -3,16 +3,17 @@ const param = {
   order: 'relevance',
   type: 'video',
   videoDefinition: 'high',
-  maxResults: 30
+  maxResults: 30,
+  categoryId: 10
 }
 const apiURL = 'https://www.googleapis.com/youtube/v3/search'
-const apiKey = 'AIzaSyDxpcGtEL7bMKiyQDjBjHXTjZXuZbs2ppk'
+const apiKey = 'AIzaSyAkh76HFpCEsXiAwi1BtnVZL5hTEFoPkdA'
 
 
 export const Api = {
 
   get (query) {
-    const url = `${ apiURL }?part=${ param.part }&order=${ param.order }&q=${ query }&type=${ param.type }+&videoDefinition=${ param.videoDefinition }&key=${ apiKey }&maxResults=${ param.maxResults }&videoCategoryId=10&videoSyndicated=true&videoEmbeddable=true&videoLicense=youtube`
+    const url = `${ apiURL }?part=${ param.part }&order=${ param.order }&q=${ query }&type=${ param.type }+&videoDefinition=${ param.videoDefinition }&key=${ apiKey }&maxResults=${ param.maxResults }&videoCategoryId=${ param.categoryId }&videoSyndicated=true&videoEmbeddable=true&videoLicense=youtube`
 
     return new Promise((resolve, reject) => {
       fetch(url)
