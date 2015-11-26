2b4c8a0c82cbcabfd46eeede0561fff808d16ec0
makotot <makoto.ttn@gmail.com>
2015-11-26 13:51:14 +0900

    * playlist component

--- src/js/components/playlist.jsx
+++ src/js/components/playlist.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { PlayerAction } from '../actions/PlayerAction'
 import { PlayListAction } from '../actions/PlayListAction'
+import { PlayListStore } from '../stores/PlayListStore'
 
 
 export class PlayList extends React.Component {
@@ -15,18 +16,40 @@ export class PlayList extends React.Component {
   }
 
   componentDidMount () {
+    PlayListStore
+      .on('update', this.updatePlayList.bind(this))
+      .on('select', this.selectPlayListItem.bind(this))
   }
 
   componentWillUnmount () {
+    PlayListStore
+      .off('change')
+      .off('select')
+  }
+
+  updatePlayList () {
+    this.setState({
+      result: PlayListStore.getAll()
+    })
+
+    const idList = PlayListStore.getAllId()
+
+    window.player.loadPlaylist(idList, 0)
+    window.player.setLoop(true);
+  }
+
+  selectPlayListItem () {
+    const idList = PlayListStore.getAllId()
+
+    window.player.loadPlaylist(idList, PlayListStore.itemIndex)
   }
 
   playItem (videoId, index) {
-    //PlayerAction.select(videoId)
     PlayListAction.selectItem(videoId, index)
   }
 
   render () {
-    const items = this.props.items || []
+    const items = PlayListStore.getAll() || []
     const itemList = items.map((item, index) => {
       return (
         <li
